03983e887a1f7a6a3927aa0feec38494f64fa322
Anh Ky Nguyen <anh163@gmail.com>
2023-02-24 11:13:45 +0700

    Changes

--- coderdex-be/routes/index.js
+++ coderdex-be/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).send("Welcome to CoderSchool!");
+  res.status(200).send("Welcome to Pokedex!");
 });
 
 const pokemonRouter = require("./pokemon.api.js");
